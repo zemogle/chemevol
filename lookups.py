4b534016995b9c9c5f3c8e16282024865d54c5b5
Haley <haley@gomez.me.uk>
2015-10-29 14:27:03 -0700

    table lifetime

--- lookups.py
+++ lookups.py
@@ -24,7 +24,31 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 '''
 from astropy.table import Table
+'''
+- lifetime: the lifetime of stars of a given mass (1st column)
+            2nd col: lifetime in Gyrs when metallicity < 0.008
+            3rd col: lifetime in Gyrs when metallicity >= 0.008
+            Values from Schaller et al 1992 (A & AS 96 269)
+'''
+lifetime =  [(0.8, 15.0, 26.0),
+            (0.9, 9.5, 15.0),
+            (1.0, 6.3, 10.0),
+            (1.5, 1.8, 2.7),
+            (2.0, 0.86, 1.1),
+            (3.0, 0.29, 0.35),
+            (4.0, 0.14, 0.16),
+            (5.0, 0.088, 0.094),
+            (7.0, 0.045, 0.043),
+            (9.0, 0.029, 0.026),
+            (12.0, 0.018, 0.016),
+            (20.0, 0.0094, 0.0081),
+            (40.0, 0.0049, 0.0043),
+            (60.0, 0.0037, 0.0034),
+            (85.0, 0.0031, 0.0028),
+            (120.0, 0.0028, 0.0026)]
+            
+t_lifetime = Table(rows=lifetime, names=('mass','lifetime_low','lifetime_high'),meta={'name': 'Lifetime'})
 
-lifetime = (
-    (0.8,26.0,15.0),
-)
+#lifetime = (
+#    (0.8,26.0,15.0),
+#)
