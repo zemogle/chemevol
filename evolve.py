5f52430232371a2cc005bd46e8d173e16212a5eb
Haley <haley@gomez.me.uk>
2015-11-06 10:12:08 -0800

    fixed gas mass to use IMF from dictionary

--- evolve.py
+++ evolve.py
@@ -43,24 +43,24 @@ class ChemModel:
         except:
             logger.error("No SFH yet")
 
-    def ejected_mass(self, t, choice):
+    def ejected_mass(self, t):
         mu = t_lifetime[-1][0]
         m = lifetime_lookup(t_lifetime,'lifetime_low_metals',t)[0]
         dm = 0.5
         em = 0.
         while m <= mu:
             m += dm
-            em += f.ejected_gas_mass(m, self.sfr(t), choice) * dm
+            em += f.ejected_gas_mass(m, self.sfr(t), self.imf_type) * dm
         print(em)
         return em
 
-    def gas_mass(self, choice):
+    def gas_mass(self):
         t = self.sfh[0][0] + 0.00001
         t_end = self.sfh[-1][0]
         dlogt=(np.log10(t_end)-np.log10(t))/100.
         mg = self.gasmass_init
         while t <= t_end:
-            dmg = - self.sfr(t) + self.ejected_mass(t, choice) + f.inflows(self.sfr(t), self.inflows).value + f.outflows(self.sfr(t), self.outflows).value
+            dmg = - self.sfr(t) + self.ejected_mass(t) + f.inflows(self.sfr(t), self.inflows).value + f.outflows(self.sfr(t), self.outflows).value
             mg += dmg * 10.**dlogt
             t += 10.**(np.log10(t)+dlogt)
             print('dMg', dmg, self.sfr(t), t)
