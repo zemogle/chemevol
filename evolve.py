523fc6c59f47c361392b350dedbe547b4f563a5b
Edward Gomez <edward@gomez.me.uk>
2015-11-05 22:25:51 -0800

    Including the initial gas mass

--- evolve.py
+++ evolve.py
@@ -45,21 +45,23 @@ class ChemModel:
 
     def ejected_mass(self, t, choice):
         mu = t_lifetime[-1][0]
-        m = lifetime_lookup(t_lifetime,'lifetime_low_metals',t)
-        dm = 0.1
+        m = lifetime_lookup(t_lifetime,'lifetime_low_metals',t)[0]
+        dm = 0.5
         em = 0.
         while m <= mu:
             m += dm
             em += f.ejected_gas_mass(m, self.sfr(t), choice) * dm
+        print(em)
         return em
 
     def gas_mass(self, choice):
-        t = self.sfh[0][0]
+        t = self.sfh[0][0] + 0.00001
         t_end = self.sfh[-1][0]
         dlogt=(np.log10(t_end)-np.log10(t))/100.
-        mg = 0.
+        mg = self.gasmass_init
         while t <= t_end:
-            t += 10.**(np.log10(t)+dlogt)
-            dmg = - self.sfr(t) + self.ejected_mass(t, choice) + f.inflows(self.sfr(t), self.inflows) + f.outflows(self.sfr(t), self.outflows)
+            dmg = - self.sfr(t) + self.ejected_mass(t, choice) + f.inflows(self.sfr(t), self.inflows).value + f.outflows(self.sfr(t), self.outflows).value
             mg += dmg * 10.**dlogt
+            t += 10.**(np.log10(t)+dlogt)
+            print('dMg', dmg, self.sfr(t), t)
         return mg
