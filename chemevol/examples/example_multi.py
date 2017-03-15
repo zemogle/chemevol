9456bbf2296cd6d39b9aa7162e491c5437f078e3
haley gomez <haley@gomez.me.uk>
2017-03-15 13:42:59 +0000

    added burst.sfh example

--- chemevol/examples/example_multi.py
+++ chemevol/examples/example_multi.py
@@ -54,6 +54,22 @@ from astropy.table import Table
 # each {} entry is per galaxy separated by comma in list
 
 inits = [
+	{	'name': 'Model_VII_test',
+		'gasmass_init': 4e10,
+		'SFH': 'burst.sfh',
+		't_end': 20.,
+		'gamma': 0,
+		'IMF_fn': 'Chab',
+		'dust_source': 'All',
+		'reduce_sn_dust': 12,
+		'destroy': True,
+		'inflows':{'metals': 0., 'xSFR': 6, 'dust': 0},
+		'outflows':{'metals': True, 'xSFR': 6,'dust': True},
+		'cold_gas_fraction': 0.5,
+		'epsilon_grain': 10000,
+		'destruct': 100}
+]
+'''
 		{	'name': 'Model_I_test',
 			'gasmass_init': 4e10,
 			'SFH': 'Milkyway.sfh',
@@ -142,11 +158,8 @@ inits = [
 			'outflows':{'metals': True, 'xSFR': 2.5,'dust': True},
 			'cold_gas_fraction': 0.5,
 			'epsilon_grain': 10000,
-			'destruct': 150}
-
-]
-
-
+			'destruct': 150},
+'''
 
 snrate = []
 all_results = []
