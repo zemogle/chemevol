a27c26f0ca34221d24af9308a20e7678c70ef69c
haley gomez <haley@gomez.me.uk>
2017-04-06 11:54:31 +0100

    playing with testing outflows

--- chemevol/examples/example_multi.py
+++ chemevol/examples/example_multi.py
@@ -60,14 +60,15 @@ inits = [
 			't_end': 20.,
 			'gamma': 0,
 			'IMF_fn': 'Chab',
-			'dust_source': 'SN+LIMS',
+			'dust_source': 'All',
 			'reduce_sn_dust': False,
 			'destroy': False,
 			'inflows':{'metals': 0., 'xSFR': 0, 'dust': 0},
-			'outflows':{'metals': False, 'xSFR': 0, 'dust': False},
+			'outflows':{'metals': True, 'xSFR': 6, 'dust': False},
 			'cold_gas_fraction': 0.5,
 			'epsilon_grain': 0,
 			'destruct': 0 },
+
 
 		{	'name': 'Model_II_test',
 			'gasmass_init': 4e10,
