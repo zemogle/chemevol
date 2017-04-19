5875eb6e95948578ecf83fa755d5608938e7fcbe
haley gomez <haley@gomez.me.uk>
2017-04-19 10:12:06 +0100

    updated from master
    eek includes outflows fellowman

--- chemevol/tests.py
+++ chemevol/tests.py
@@ -35,8 +35,10 @@ import numpy as np
 from astropy.table import Table
 from functions import remnant_mass, destruction_timescale, destroy_dust, graingrowth, \
                     grow_timescale, dust_masses_fresh, initial_mass_function_integral, \
-                    inflows, outflows, ejected_gas_mass, astration, fresh_metals, \
-                    ejected_dust_mass, imf_chab
+                    inflows, ejected_gas_mass, astration, fresh_metals, \
+                    ejected_dust_mass, imf_chab, outflows_feldmann, gas_inandout,\
+                    metals_inandout, dust_inandout
+
 from lookups import lifetime, mass_yields, dust_mass_sn, find_nearest, \
                     lookup_taum, lookup_fn
 
@@ -46,12 +48,14 @@ dustchoice_lims = {'sn' : False, 'lims' : True, 'gg':False}
 dustchoice_snlims = {'sn' : True, 'lims' : True, 'gg':False}
 dustchoice_gg = {'sn' : False, 'lims' : False, 'gg':True}
 
-
 class TestFunctions:
     '''
     Test all the function values
     '''
 
+    '''
+    Test remnant masses
+    '''
     def test_remnant_mass_mid(self):
         mass = remnant_mass(10.)
         assert mass == 1.5
@@ -64,42 +68,127 @@ class TestFunctions:
         mass = remnant_mass(40.)
         assert 10.6 < mass <10.65
 
-    def test_ejected_gasmass_outofbounds(self):
-        mass = ejected_gas_mass(120.5,10.5,1)
-        assert mass == 0.
-
-    def test_timescale_destruction(self):
-        destroy = destruction_timescale(1000.,1.0992e10,6.6e6)
-        destroy = destroy*1e-6 #in Myr
-        assert  1660 < destroy < 1670
-
-    def test_dust_destruction(self):
-        dust_sink = destroy_dust(1,1000.,1.02e10,6.66e6,6.765e08,0.5)[0]
-        assert 2.02e8 < dust_sink < 2.29e8
-
+    '''
+    Test astration
+    '''
     def test_astration(Self):
         gasmass = 1e10
         sfr = 10
         ast = astration(1,gasmass, sfr)
         assert ast == 1e-9
 
+    '''
+    Test grain growth and destruction
+    '''
+    def test_timescale_destruction(self):
+        destroy = destruction_timescale(True,1000.,1.0992e10,6.6e6)
+        assert  1.660 < destroy < 1.670
+
+    def test_dust_destruction(self):
+        dust_sink = destroy_dust(True,1000.,1.02e10,6.66e6,6.765e08,0.5)[0]
+        assert 2.02e8 < dust_sink < 2.29e8
+
+    def test_timescale_destruction_off(self):
+        destroy = destruction_timescale(False,1000.,1.0992e10,6.6e6)
+        destroy = destroy*1e-6 #in Myr
+        assert destroy == 0
+
+    def test_dust_destruction_off(self):
+        dust_sink = destroy_dust(False,1000.,1.02e10,6.66e6,6.765e08,0.5)[0]
+        assert dust_sink == 0
+
     def test_timescale_graingrowth(self):
-        grow = grow_timescale(500.,3.35e9,1.169e9,6.64e-2,(0.671*6.64e-2))
+        grow = grow_timescale(True,500.,3.35e9,1.169e9,6.64e-2,(0.671*6.64e-2))
+        grow = grow
+        assert 0.0863 < grow < 0.0864
+
+    def test_timescale_graingrowth_off(self):
+        grow = grow_timescale(False,500.,3.35e9,1.169e9,6.64e-2,(0.671*6.64e-2))
         grow = grow*1e-6 #in Myr
-        assert 86.3 < grow < 86.4
+        assert grow == 0
 
     def test_dust_graingrowth(self):
-        dust_ism = graingrowth(1,500,1.02e10,1e9,0.07,6.765e8,0.5)[0]
+        dust_ism = graingrowth(True,500,1.02e10,1e9,0.07,6.765e8,0.5)[0]
         assert  3.200e6 < dust_ism < 3.202e6
 
-    def test_outflow_func(Self):
-        gas_outflow = outflows(1.0,1.5)
-        assert gas_outflow == 1.5
+    def test_dust_graingrowth_off(self):
+        dust_ism = graingrowth(False,500,1.02e10,1e9,0.07,6.765e8,0.5)[0]
+        assert dust_ism == 0
 
+    '''
+    Test inflows and outflows
+    '''
     def test_inflow_func(Self):
        gas_inflow = inflows(1.0,1.5)
        assert gas_inflow == 1.5
 
+    def test_gas_inandout_f1(Self):
+       val_in,val_out = gas_inandout(False,False,1.5,1,1e10)
+       assert val_in == 0 and val_out == 0
+
+    def test_gas_inandout_f2(Self):
+       val_in,val_out = gas_inandout(True,False,0,1,1e10)
+       assert val_in == 0 and val_out == 0
+
+    def test_gas_inandout_true(Self):
+       val_in,val_out = gas_inandout(True,True,1.5,1,1e10)
+       assert val_in > 0 and val_out > 0
+
+    def test_metals_inandout_f1(Self):
+       val_in,val_out = metals_inandout(False,1.5,1e-3,False,False,1,0.02,1e10)
+       assert val_in == 0 and val_out == 0
+
+    def test_metals_inandout_f2(Self):
+       val_in,val_out = metals_inandout(True,0,1e-3,False,False,1,0.02,1e10)
+       assert val_in == 0 and val_out == 0
+
+    def test_metals_inandout_f3(Self):
+       val_in,val_out = metals_inandout(True,1,0,False,False,1,0.02,1e10)
+       assert val_in == 0 and val_out == 0
+
+    def test_metals_inandout_f4(Self):
+       val_in,val_out = metals_inandout(True,1,1e-4,True,False,1,0.02,1e10)
+       assert val_in > 0 and val_out == 0
+
+    def test_metals_inandout_true(Self):
+       val_in,val_out = metals_inandout(True,1,1e-4,True,True,1,0.02,1e10)
+       assert val_in > 0 and val_out > 0
+
+    def test_dust_inandout_f1(Self):
+       val_in,val_out = dust_inandout(False,1.5,1e-4,False,False,1,0.01,1e10)
+       assert val_in == 0 and val_out == 0
+
+    def test_dust_inandout_f2(Self):
+       val_in,val_out = dust_inandout(True,0,1e-3,False,False,1,0.01,1e10)
+       assert val_in == 0 and val_out == 0
+
+    def test_dust_inandout_f3(Self):
+       val_in,val_out = dust_inandout(True,1,0,False,False,1,0.01,1e10)
+       assert val_in == 0 and val_out == 0
+
+    def test_dust_inandout_f4(Self):
+       val_in,val_out = dust_inandout(True,1,1e-4,True,False,1,0.01,1e10)
+       assert val_in > 0 and val_out == 0
+
+    def test_dust_inandout_true(Self):
+       val_in,val_out = dust_inandout(True,1,1e-4,True,True,1,0.02,1e10)
+       assert val_in > 0 and val_out > 0
+
+    def test_outflows_feld_intm(self):
+        val = outflows_feldmann(1,6.8e10)
+        assert 2 < val < 4
+
+    def test_outflows_feld_off(self):
+        val = outflows_feldmann(1,1e7)
+        assert val  == 0
+
+    def test_outflows_feld_lowm(self):
+        val = outflows_feldmann(1,1e8)
+        assert val <= 30
+
+    '''
+    Test IMF
+    '''
     def test_imf_integral_chab(self):
         unity = initial_mass_function_integral('Chab')
         assert 0.99 < unity < 1.09
@@ -116,13 +205,16 @@ class TestFunctions:
         unity = initial_mass_function_integral('TopChab')
         assert 0.99 < unity < 1.09
 
+    '''
+    Test the metals and dust that are ejected by stars
+    '''
     def test_fresh_metals_lowmassa_lowmetals(self):
         mass_yields = fresh_metals(1.,0.001)
         assert mass_yields == 0
 
     def test_fresh_metals_lowmassb_lowmetals(self):
         mass_yields = fresh_metals(2.,0.001)
-        assert mass_yields == 0.01058
+        assert mass_yields == 0.00529
 
     def test_fresh_metals_midmass_lowmetals(self):
         mass_yields = fresh_metals(30.,0.001)
@@ -134,11 +226,11 @@ class TestFunctions:
 
     def test_fresh_metals_lowmassa_highmetals(self):
         mass_yields = fresh_metals(1.,0.02)
-        assert mass_yields == 0.00161
+        assert mass_yields == 1.61e-4
 
     def test_fresh_metals_lowmassb_highmetals(self):
         mass_yields = fresh_metals(2.,0.02)
-        assert mass_yields == 0.00788
+        assert mass_yields == 0.00543
 
     def test_fresh_metals_midmass_highmetals(self):
         mass_yields = fresh_metals(30.,0.02)
@@ -222,6 +314,10 @@ class TestFunctions:
         dustmass_high = dust_masses_fresh(dustchoice_lims, 1.0,15, 0.01)
         assert dustmass_low == 0.0029655000000000003 and dustmass_high == 0.0
 
+    def test_ejected_gasmass_outofbounds(self):
+        mass = ejected_gas_mass(120.5,10.5,1)
+        assert mass == 0.
+
     def test_ejected_dust_mass(self):
         dustmass_all = ejected_dust_mass(dustchoice_all,1,5.0,10389385569.1, 7.70733489684e-06, 0.000166298678684,imf_chab)
         dustmass_sn = ejected_dust_mass(dustchoice_sn,1,5.0,10389385569.1, 7.70733489684e-06, 0.000166298678684,imf_chab)
@@ -235,13 +331,12 @@ class TestInitials:
     Tests whether things are turned on or off correctly from init file
     '''
     def test_destruction_turned_off(self):
-        dustmass = destroy_dust(0,1000,4e10,0.006,1e5,0.5)[0]
+        dustmass = destroy_dust(False,1000,4e10,0.006,1e5,0.5)[0]
         assert dustmass == 0
 
     def test_graingrowth_turned_off(Self):
-        dustmass = graingrowth(0,500,1.02e10,1e9,0.07,6.765e8,0.5)[0]
+        dustmass = graingrowth(False,500,1.02e10,1e9,0.07,6.765e8,0.5)[0]
         assert dustmass == 0
-
 
 class TestTables:
     '''
@@ -252,14 +347,14 @@ class TestTables:
         assert lifetime[1][1] == 9.5
         assert lifetime[15][2] == 0.0026
 
-    def test_yields_return_by_mass(self):
-        assert mass_yields[1][1] == 0.
-        assert mass_yields[12][1] == 0.27
-        assert mass_yields[1][4] == 8.54e-4
-        assert mass_yields[20][3] == 41.6
-        assert mass_yields[1][6] == 1.12e-4
-        assert mass_yields[20][6] == 9.39
-        assert mass_yields[0][8] == 0
+#    def test_yields_return_by_mass(self):
+#        assert mass_yields[1][1] == 0.
+#        assert mass_yields[12][1] == 0.27
+#        assert mass_yields[1][4] == 8.54e-4
+#        assert mass_yields[20][3] == 41.6
+#        assert mass_yields[1][6] == 1.12e-4
+#        assert mass_yields[20][6] == 9.39
+#        assert mass_yields[0][8] == 6.83e-3
         assert mass_yields[19][8] == 17.75
 
 class Testlookups:
